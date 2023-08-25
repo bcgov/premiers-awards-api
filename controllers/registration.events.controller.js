755c9cb3b853fb80e4a98b641a4a47cc74893a20
DenisKorkut-bcgov <125493524+DenisKorkut-bcgov@users.noreply.github.com>
2023-08-25 15:52:44 -0700

    Added getAllRegistrations for registrar's org

--- controllers/registration.events.controller.js
+++ controllers/registration.events.controller.js
@@ -21,8 +21,19 @@ const RegistrationModel = require("../models/registration.events.model.js");
 
 exports.getAllRegistrations = async (req, res, next) => {
   try {
-    const registrations = await RegistrationModel.find({});
-    return res.status(200).json(registrations);
+    if (!res.locals.user) return next(new Error('noAuth'));
+    const {roles=[]} = res.locals.user || {};
+    if (roles.includes('administrator') || roles.includes('super-administrator')) {
+      const registrations = await RegistrationModel.find({});
+      return res.status(200).json(registrations);
+    }
+    else if (roles.includes('registrar')){
+      const registration = await RegistrationModel.find({ guid: res.locals.user.guid});
+      const organization = registration[0].organization;
+      const registrations = await RegistrationModel.find({organization: organization});
+      return res.status(200).json(registrations);
+    }
+
   } catch (err) {
     console.error(err);
     return next(err);
@@ -55,6 +66,21 @@ exports.getRegistration = async (req, res, next) => {
       });
     }
 
+    return res.status(200).json(registration);
+  } catch (err) {
+    console.error(err);
+    return next(err);
+  }
+};
+
+exports.getMinistryRegistrations = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const registration = await RegistrationModel.find({
+      "users.guid": { $regex: id },
+    });
+    const ministry = registration.organization;
+    //const registrations = wait RegistrationModel.find({})
     return res.status(200).json(registration);
   } catch (err) {
     console.error(err);
