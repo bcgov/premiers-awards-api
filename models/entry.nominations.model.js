96250b1d8375822f742ae151b428e847e228aa13
DenisKorkut-bcgov <125493524+DenisKorkut-bcgov@users.noreply.github.com>
2024-01-15 10:46:29 -0800

    Content changes (round 2)
    Added branch and division to NominatorSchema

--- models/entry.nominations.model.js
+++ models/entry.nominations.model.js
@@ -5,7 +5,7 @@
  * MIT Licensed
  */
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 /**
@@ -22,7 +22,7 @@ const Schema = mongoose.Schema;
  */
 
 const PartnerSchema = new Schema({
-    organization    : String
+  organization: String,
 });
 
 /**
@@ -30,10 +30,12 @@ const PartnerSchema = new Schema({
  */
 
 const NominatorSchema = new Schema({
-    firstname       : String,
-    lastname        : String,
-    title           : String,
-    email           : String
+  firstname: String,
+  lastname: String,
+  title: String,
+  email: String,
+  branch: String,
+  division: String,
 });
 
 /**
@@ -41,8 +43,8 @@ const NominatorSchema = new Schema({
  */
 
 const LocationSchema = new Schema({
-    address         : String,
-    city            : String
+  address: String,
+  city: String,
 });
 
 /**
@@ -50,152 +52,164 @@ const LocationSchema = new Schema({
  */
 
 const NominationSchema = new Schema(
-    {
-        seq: {
-            type: Number
-        },
-        category: {
-            type: String,
-            required: true
-        },
-        year: {
-            type: Number,
-        },
-        guid: {
-            type: String,
-            required: true
-        },
-        owner: {
-            type: Schema.Types.ObjectId,
-            ref: 'User',
-            required: true
-        },
-        submitted: {
-            type: Boolean,
-            required: true
-        },
-        filePaths: {
-            nomination: {
-                type: String,
-                default: '',
-            },
-            merged: {
-                type: String,
-                default: '',
-            },
-        },
-        organizations: [{
-            type: String,
-            required: function() { return this.submitted }
-        }],
-        title: {
-            type: String
-        },
-        nominee: {
-            firstname: {
-                type: String,
-                default: '',
-            },
-            lastname: {
-                type: String,
-                default: '',
-            },
-            organization: {
-                type: String,
-                default: '',
-            }
-        },
-        nominees: {
-            type: Number,
-            default: 0,
-        },
-        partners: [PartnerSchema],
-        contacts: {
-            primary: {
-                firstname: {
-                    type: String,
-                    default: '',
-                },
-                lastname: {
-                    type: String,
-                    default: '',
-                },
-                email: {
-                    type: String,
-                    default: '',
-                },
-                phone: {
-                    type: String,
-                    default: '',
-                }
-            },
-            video: {
-                firstname: {
-                    type: String,
-                    default: '',
-                },
-                lastname: {
-                    type: String,
-                    default: '',
-                },
-                email: {
-                    type: String,
-                    default: '',
-                },
-                phone: {
-                    type: String,
-                    default: '',
-                },
-                locations: [LocationSchema]
-            }
-        },
-        nominators: [NominatorSchema],
-        acknowledgment: {
-            type: Boolean,
-            default: false,
-            required: function() { return this.submitted }
-        },
-        evaluation: {
-            summary: {
-                type: String,
-                default: '',
-            },
-            context: {
-                type: String,
-                default: '',
-            },
-            complexity: {
-                type: String,
-                default: '',
-            },
-            approach: {
-                type: String,
-                default: '',
-            },
-            valuing_people: {
-                type: String,
-                default: '',
-            },
-            commitment: {
-                type: String,
-                default: '',
-            },
-            contribution: {
-                type: String,
-                default: '',
-            },
-            impact: {
-                type: String,
-                default: '',
-            }
-        },
-        attachments: [{
-            type: Schema.Types.ObjectId,
-            ref: 'Attachment'
-        }]
+  {
+    seq: {
+      type: Number,
     },
-    { timestamps: true }
+    category: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: Number,
+    },
+    guid: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    submitted: {
+      type: Boolean,
+      required: true,
+    },
+    filePaths: {
+      nomination: {
+        type: String,
+        default: "",
+      },
+      merged: {
+        type: String,
+        default: "",
+      },
+    },
+    organizations: [
+      {
+        type: String,
+        required: function () {
+          return this.submitted;
+        },
+      },
+    ],
+    title: {
+      type: String,
+    },
+    nominee: {
+      firstname: {
+        type: String,
+        default: "",
+      },
+      lastname: {
+        type: String,
+        default: "",
+      },
+      organization: {
+        type: String,
+        default: "",
+      },
+    },
+    nominees: {
+      type: Number,
+      default: 0,
+    },
+    partners: [PartnerSchema],
+    contacts: {
+      primary: {
+        firstname: {
+          type: String,
+          default: "",
+        },
+        lastname: {
+          type: String,
+          default: "",
+        },
+        email: {
+          type: String,
+          default: "",
+        },
+        phone: {
+          type: String,
+          default: "",
+        },
+      },
+      video: {
+        firstname: {
+          type: String,
+          default: "",
+        },
+        lastname: {
+          type: String,
+          default: "",
+        },
+        email: {
+          type: String,
+          default: "",
+        },
+        phone: {
+          type: String,
+          default: "",
+        },
+        locations: [LocationSchema],
+      },
+    },
+    nominators: [NominatorSchema],
+    acknowledgment: {
+      type: Boolean,
+      default: false,
+      required: function () {
+        return this.submitted;
+      },
+    },
+    evaluation: {
+      summary: {
+        type: String,
+        default: "",
+      },
+      context: {
+        type: String,
+        default: "",
+      },
+      complexity: {
+        type: String,
+        default: "",
+      },
+      approach: {
+        type: String,
+        default: "",
+      },
+      valuing_people: {
+        type: String,
+        default: "",
+      },
+      commitment: {
+        type: String,
+        default: "",
+      },
+      contribution: {
+        type: String,
+        default: "",
+      },
+      impact: {
+        type: String,
+        default: "",
+      },
+    },
+    attachments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Attachment",
+      },
+    ],
+  },
+  { timestamps: true }
 );
 
-const EntryNominationsModel = mongoose.model('Nomination', NominationSchema, 'nominations');
+const EntryNominationsModel = mongoose.model(
+  "Nomination",
+  NominationSchema,
+  "nominations"
+);
 module.exports = EntryNominationsModel;
