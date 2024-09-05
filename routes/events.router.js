2e00528d87f2bc800b1cdfcc658d05bdaa88107a
paulbothma-govbcca <paul.bothma@gov.bc.ca>
2024-09-05 13:56:29 -0700

    Added PDF download for table layout

--- routes/events.router.js
+++ routes/events.router.js
@@ -112,6 +112,8 @@ router.get(
   authorizeAdmin,
   tableController.getTableGuests
 );
+router.get("/seating/pdf", authorizeAdmin, tableController.getPdfLayout);
+
 router.get("/seating/:id/", authorizeAdmin, tableController.getTable);
 
 //settings routes
