f0e3ba4f77980d779093b9b191edf4ed3f709cd4
DenisKorkut-bcgov <125493524+DenisKorkut-bcgov@users.noreply.github.com>
2025-12-01 16:43:58 -0800

    Fix deprecated mongoose.connect

--- db.js
+++ db.js
@@ -6,50 +6,66 @@
  */
 
 const mongoose = require("mongoose");
-require('dotenv').config();
+require("dotenv").config();
 
 // Database configuration settings
-const protocol = 'mongodb://'
+const protocol = "mongodb://";
 const databaseHost = process.env.DATABASE_HOST;
 const databasePort = process.env.DATABASE_PORT;
 const databaseUser = process.env.DATABASE_USER;
 const databasePassword = process.env.DATABASE_PASSWORD;
 // const databaseConnectionOpts = process.env.DATABASE_CONNECTION_OPTIONS;
 const databaseName = process.env.DATABASE_NAME;
-const authSource = process.env.DATABASE_AUTH || databaseName
+const authSource = process.env.DATABASE_AUTH || databaseName;
 const connectionURL = `${protocol}${databaseHost}:${databasePort}/${databaseName}`;
 
 // DEBUG db connection URL
-if (process.env.DEBUG === 'true') {
-  console.log('\n\n=== DEBUG ===')
+if (process.env.DEBUG === "true") {
+  console.log("\n\n=== DEBUG ===");
   console.log(`Connection URL:\t\t${connectionURL}`);
-  console.log(`\t- User:\t\t${databaseUser}\n\t- Password:\t${databasePassword}`);
-  console.log('=== \n\n')
+  console.log(
+    `\t- User:\t\t${databaseUser}\n\t- Password:\t${databasePassword}`
+  );
+  console.log("=== \n\n");
 }
 
 // create db connection
-mongoose.connect(connectionURL, {
-  auth: {
-    username: databaseUser,
-    password: databasePassword
-  },
-  directConnection: true,
-  authSource: authSource,
-  useUnifiedTopology: true,
-  useNewUrlParser: true
-}, (err)=>{ if (err) console.error(err); });
+mongoose
+  .connect(connectionURL, {
+    auth: {
+      username: databaseUser,
+      password: databasePassword,
+    },
+    directConnection: true,
+    authSource: authSource,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log(`Connected to MongoDB on port ${databasePort}.`);
+    console.log(`\t- Host: ${databaseHost}`);
+    console.log(`\t- Database: ${databaseName}`);
+    console.log(`\t- Auth: ${authSource}`);
+    console.log(`\t- User: ${databaseUser}`);
+    console.log(`============================================`);
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+//}, (err)=>{ if (err) console.error(err); });
 
 // Bind connection to error event (to get notification of connection errors)
-mongoose.connection.on('error',
-  console.error.bind(console, 'MongoDB connection error:')
+mongoose.connection.on(
+  "error",
+  console.error.bind(console, "MongoDB connection error:")
 );
 
-// Connect to MongoDB
-mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB on port ${databasePort}.`);
-  console.log(`\t- Host: ${databaseHost}`);
-  console.log(`\t- Database: ${databaseName}`);
-  console.log(`\t- Auth: ${authSource}`);
-  console.log(`\t- User: ${databaseUser}`);
-  console.log(`============================================`);
-});
+// // Connect to MongoDB
+// mongoose.connection.on("connected", () => {
+//   console.log(`Connected to MongoDB on port ${databasePort}.`);
+//   console.log(`\t- Host: ${databaseHost}`);
+//   console.log(`\t- Database: ${databaseName}`);
+//   console.log(`\t- Auth: ${authSource}`);
+//   console.log(`\t- User: ${databaseUser}`);
+//   console.log(`============================================`);
+// });
